ba211b4cf775924a3b2efd89456dec683ad23b6a
s sh <chrpsh@gmail.com>
2022-01-20 23:27:25 +0400

    fix actions, add turns

--- main.lua
+++ main.lua
@@ -2,16 +2,25 @@ function love.load()
 	Object = require "classic"
 	tick = require "tick"
 	require "unit"
+	require "actions"
 	
-	txt = love.graphics.newFont("trnsgndr.ttf", 24)
+	txt = love.graphics.newFont("trnsgndr.ttf", 22)
 	window_width = love.graphics.getWidth()
     window_height = love.graphics.getHeight()
 
-    --'74K3M1'	--'K45UM1'
-
     unit = {}
-    unit[0] = Unit(0,'74K3M1')
-    unit[1] = Unit(1,'K45UM1')
+    unit[0] = Unit(0,'M1ZUH0')
+    unit[1] = Unit(1,'N0Z0M1')
+
+    actions = Actions() 
+	
+	--[[actions = {}
+    actions[0] = Actions(0) 
+    actions[1] = Actions(1)]]
+
+    turn = 0
+    
+    tmp_show = 0
 
 	math.randomseed(os.time())
 end
@@ -35,37 +44,61 @@ function setColor(name)
 end
 
 function love.keypressed(key)
+	actions:keyPressed(key)
+	--actions[0]:keyPressed(key)
+	--actions[1]:keyPressed(key)
+
 	if key == "space" then
 		love.load()
 	elseif key == "return" then
-
+		--console(1)
+		--tmp_show = 1
+		if turn == 0 then turn = 1 
+		elseif turn == 1 then turn = 0 
+		end
+		actions:changed(turn)
+		--actions[1]:changed(turn)
 	end
 end
 
+function console(unit, hov)
+	local line = unit[0].line
+
+	--unit_num --hov_num
+
+	local name = unit[unit].name
+	local str = {'TOOK MEDS', 'USED PREGABALIN', 'USED SEDATIVE', 'TRIED TO CRY'}
+
+	love.graphics.print(name .. ' ' .. str[hov], 0, window_height - line*2-4)
+
+	local w = txt:getWidth('ENTER')
+	love.graphics.print('ENTER', window_width-w, window_height - line*1-4)
+end
 
 function love.update(dt)
 	tick.update(dt)
+	actions:update(dt)
+	--actions[0]:update(dt)
+	--actions[1]:update(dt)
 end
 
 
 function love.draw()
-	setColor('blue')
-	love.graphics.rectangle('fill', 0, 0, window_width/3, window_height)
-	setColor('white')
 	unit[0]:draw()
 	unit[1]:draw()
-	local width = window_width/2
-	local x = window_width/3
-	local y = 0
-	local lh = 22
-	local dmg = math.random(1,5)
-	--love.graphics.printf(unit[0].name .. ' IS DIZZY, DEALS ' .. dmg .. ' pain', x, y, width)
-	love.graphics.print(unit[0].name .. ' TIRED OF THEMSELF', x, y)
-	love.graphics.print('DEALS ' .. dmg .. ' PAIN', x, y+lh*1)
+	actions:draw()
+	--actions[turn]:draw()
 
-	love.graphics.print(unit[1].name .. ' NAUSEOUS', x, y+lh*3)
-	--love.graphics.print('VOMITS', x, y+lh*4)
-	love.graphics.print('RECIEVES ' .. dmg .. ' PAIN', x, y+lh*4)
-	--love.graphics.print(unit[1].name .. ' NAUSEOUS / DEALS ' .. dmg .. ' PAIN', x, y+lh*2)
-	--love.graphics.printf('DEALS ' .. dmg .. ' PAIN', x, y+lh, width)
+	--local hov = unit[turn].hov
+	--console(turn, hov)
+
+	--local line = unit[0].line
+
+	--[[if tmp_show == 1 then
+		love.graphics.print(unit[0].name .. ' TAKES MEDS, THEY WORK', 0, window_height - line*2-4)
+		love.graphics.print('CONFUSION −1, NAUSEA −1', 0, window_height - line*1-4)
+
+		local w = txt:getWidth('ENTER')
+		love.graphics.print('ENTER', window_width-w, window_height - line*1-4)
+	end]]
 end
