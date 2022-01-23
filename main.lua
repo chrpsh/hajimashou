c71bee89e5f544bcb8daa3a08f8920eef0c90a79
s sh <chrpsh@gmail.com>
2022-01-24 01:43:12 +0400

    base actions, stst change, too much design

--- main.lua
+++ main.lua
@@ -18,7 +18,13 @@ function love.load()
     actions[0] = Actions(0) 
     actions[1] = Actions(1)]]
 
+    log = {}
+    log.str = {}
+    log.count = 1
+
     turn = 0
+
+    console_show = false
     
     tmp_show = 0
 
@@ -53,26 +59,120 @@ function love.keypressed(key)
 	elseif key == "return" then
 		--console(1)
 		--tmp_show = 1
-		if turn == 0 then turn = 1 
-		elseif turn == 1 then turn = 0 
+
+		--showing actions
+		--choosing action
+		--hide actions
+		--show result
+		--enter
+		--showing actions
+
+		--if actions.active then
+		if actions.show and not console_show then
+			actions.show = false
+			console_show = true
+
+			--log update
+				local hov = actions.hov
+				local name = unit[turn].name
+				local act = {'TAKES MEDS', 'USES PREGABALIN', 'USES SEDATIVE', 'TRIES TO CRY'}
+				local stat = {'NAUSEA', 'CONFUSION', 'INSOMNIA'}
+
+				log.str[log.count] = name .. ' ' .. act[hov]
+				log.count = log.count + 1
+
+				if hov == 1 then
+					--confusion -1, nausea -1
+					log.str[log.count] = stat[2] .. ' −1 ' .. stat[1] .. ' −1 '
+					--if unit[turn].st['nausea'] > 0 then
+						unit[turn].st['nausea'] = unit[turn].st['nausea'] - 1
+					--end
+					--if unit[turn].st['confusion'] > 0 then
+						unit[turn].st['confusion'] = unit[turn].st['confusion'] - 1
+					--end
+				elseif hov == 2 then
+					--actually feels really good
+					log.str[log.count] = 'IT FEELS REALLY GOOD'
+				elseif hov == 3 then
+					--confusion -1, insomnia -1
+					log.str[log.count] = stat[2] .. ' −1 ' .. stat[3] .. ' −1 '
+					--if unit[turn].st['confusion'] > 0 then
+						unit[turn].st['confusion'] = unit[turn].st['confusion'] - 1
+					--end
+					--if unit[turn].st['insomnia'] > 0 then
+						unit[turn].st['insomnia'] = unit[turn].st['insomnia'] - 1
+					--end
+				elseif hov == 4 then
+					--can't
+					--just can't
+					log.str[log.count] = 'CAN’T, JUST CAN’T'
+					--log.count = log.count + 1
+					--log.str[log.count] = 'JUST CANT'
+				end
+
+				log.count = log.count + 1
+				--log.str[log.count] = '——————————————————————'
+				--log.count = log.count + 1
+			
+			--/log update
+
+		else
+			--[[if turn == 0 then turn = 1 
+			elseif turn == 1 then turn = 0 
+			end]]
+
+			change_turn()
+
+			actions:changed(turn)
+			actions.show = true
+			console_show = false
 		end
-		actions:changed(turn)
+		--end
+
+		--show console
+
+		
+		
 		--actions[1]:changed(turn)
 	end
 end
 
-function console(unit, hov)
-	local line = unit[0].line
+function change_turn()
+	unit[turn].st.last['nausea'] = unit[turn].st['nausea']
+	unit[turn].st.last['confusion'] = unit[turn].st['confusion']
+	unit[turn].st.last['insomnia'] = unit[turn].st['insomnia']
+
+	if turn == 0 then 
+		turn = 1 
+	elseif turn == 1 then 
+		turn = 0 
+	end
+end
+
+function console()--num, hov)
+	local line = unit[turn].line
+
+	--action_1
+		--name
+		--action
+	--log_1
+		--name -- action
 
 	--unit_num --hov_num
 
-	local name = unit[unit].name
-	local str = {'TOOK MEDS', 'USED PREGABALIN', 'USED SEDATIVE', 'TRIED TO CRY'}
+	for i,v in ipairs(log.str) do
+		love.graphics.print(log.str[i], window_width/3, -2+line*(i-1))
+	end
 
-	love.graphics.print(name .. ' ' .. str[hov], 0, window_height - line*2-4)
 
-	local w = txt:getWidth('ENTER')
-	love.graphics.print('ENTER', window_width-w, window_height - line*1-4)
+
+	--local name = unit[num].name
+	--local str = {'TOOK MEDS', 'USED PREGABALIN', 'USED SEDATIVE', 'TRIED TO CRY'}
+
+	--love.graphics.print(name .. ' ' .. str[hov], window_width/3, -2)--window_height - line*1-4)
+
+	--local w = txt:getWidth('ENTER')
+	--love.graphics.print('ENTER', window_width-w, window_height - line*1-4)
 end
 
 function love.update(dt)
@@ -80,17 +180,33 @@ function love.update(dt)
 	actions:update(dt)
 	--actions[0]:update(dt)
 	--actions[1]:update(dt)
+	if turn == 1 then
+		unit[1].arrw = '→'
+		unit[0].arrw = ''
+	elseif turn == 0 then
+		unit[0].arrw = '→'
+		unit[1].arrw = ''
+	end
 end
 
 
 function love.draw()
+	setColor('blue')
+	love.graphics.rectangle('fill', 0, 0, window_width/3, window_height)
+	setColor('white')
 	unit[0]:draw()
 	unit[1]:draw()
-	actions:draw()
+	
+	if actions.show then
+		actions:draw()
+	end
 	--actions[turn]:draw()
 
-	--local hov = unit[turn].hov
-	--console(turn, hov)
+	--local hov = actions.hov
+	--if console_show then
+	--	console(turn, hov)
+		console()
+	--end
 
 	--local line = unit[0].line
 
--- unit.lua
+++ unit.lua
@@ -8,21 +8,53 @@ function Unit:new(num, string)
 	self.st['confusion'] = math.random(1,3)
 	self.st['insomnia'] = math.random(1,2)
 	self.line = 20
-	self.x = 0 + window_width/2*num
-	self.y = -2
+	self.x = 0 --+ window_width/2*num
+	self.y = self.line*5*num -2
+	self.arrw = ''
+
+	self.st.last = {}
+	self.st.last['nausea'] = self.st['nausea']
+	self.st.last['confusion'] = self.st['confusion']
+	self.st.last['insomnia'] = self.st['insomnia']
+
+	--self.st.change = {}
 end
 
 
 function Unit:update(dt)
 end
 
+function Unit:diff(str)
+	if self.st[str] ~= self.st.last[str] then
+		if self.st[str] > self.st.last[str] then
+			--self.st.change[str] = '+' .. self.st[str] - self.st.last[str]
+			return ' +' .. self.st[str] - self.st.last[str]
+		else
+			--self.st.change[str] = '−' .. self.st.last[str] - self.st[str]
+			return ' −' .. self.st.last[str] - self.st[str]
+		end
+	end
+	return ''
+end
 
 function Unit:draw()
 	love.graphics.setFont(txt)
-	love.graphics.print(self.name, self.x, self.y)
-	love.graphics.print('NAUSEA ' .. self.st['nausea'], self.x, self.y+self.line)
-	love.graphics.print('CONFUSION ' .. self.st['confusion'], self.x, self.y+self.line*2)
-	love.graphics.print('INSOMNIA ' .. self.st['insomnia'], self.x, self.y+self.line*3)
+	love.graphics.print(self.arrw .. self.name, self.x, self.y)
+	love.graphics.print('NAUSEA ' .. self.st['nausea'] .. self:diff('nausea'), self.x, self.y+self.line)
+	love.graphics.print('CONFUSION ' .. self.st['confusion'] .. self:diff('confusion'), self.x, self.y+self.line*2)
+	love.graphics.print('INSOMNIA ' .. self.st['insomnia'] .. self:diff('insomnia'), self.x, self.y+self.line*3)
+
+
+	--[[local str = 'nausea'
+	love.graphics.print(self:diff(str), self.x, self.y+self.line*8)
+	
+
+	if self.st[str] == nil then
+		love.graphics.print('FUCK YOU .STAT', self.x, self.y+self.line*9)
+	end
+	if self.st.last[str] == nil then
+		love.graphics.print('FUCK YOU .STAT.LAST', self.x, self.y+self.line*10)
+	end]]
 
 	--[[local w = txt:getWidth('CONFUSION') + 10
 	love.graphics.print('NAUSEA', self.x, self.y+self.line)
